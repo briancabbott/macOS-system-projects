00edf0c04ce30fc8dca7e7d54f27f3e3ccf60663
Brian Abbott <briancabbott@gmail.com>
2022-12-21 22:22:30 -0800

    adding macos-defaults project

---
+++ Resources/Code/macos-defaults/build/handlebars-helpers.js
@@ -0,0 +1,10 @@
+const Handlebars = require('handlebars')
+const slugify = require('slugify')
+
+Handlebars.registerHelper('metaTag', (string) => {
+  return JSON.stringify(string.replace(/"/g, '&quot;'))
+})
+
+Handlebars.registerHelper('slugify', (string) =>
+  slugify(string, { lower: true })
+)
